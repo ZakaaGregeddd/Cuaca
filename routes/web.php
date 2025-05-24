d3dbeea479d9e8d05ff7de481badb27a2562576e
ZakaaGregeddd <09021182328014@student.unsri.ac.id>
2025-05-24 14:33:07 +0700

    wrong

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/about', [AboutController::class, 'index']);
 });
 
-// Rute untuk RESTful API
 Route::get('/api/weather-history', [WeatherController::class, 'getWeatherHistory']);
 
 require __DIR__.'/auth.php';
